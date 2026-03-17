7117539eb207c2fcaa37e3a3dd5a12ee51c53533
labsfiap <patrickfpais@hotmail.com>
2026-03-16 22:42:55 -0300

    Aula 2 de python (variaveis)

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,17 @@
+nome = input("Digite seu nome: ") # str
+
+print(f'Bem-vindo(a) {nome}')
+
+
+dia = int(input("Digite o dia do seu nascimento (DD): "))
+mes = int(input("Digite o mes do seu nascimento (MM): "))
+ano = int(input("Digite o ano do seu nascimento (AAAA): "))
+
+print(f'Sua data de nascimento é: {dia}/{mes}/{ano}')
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+total = n1 + n2
+
+print(f'A soma desses números são: {total}')
---
+++ desafios/desafio_aula2_1603.py
@@ -0,0 +1,27 @@
+nome = input("Digite seu primeiro nome: ") # str
+nome2 = input("Digite seu sobrenome: ") # str
+
+
+print(f'Bem-vindo(a) {nome} {nome2}')
+
+
+dia = int(input("Digite o dia do seu nascimento (DD): "))
+mes = int(input("Digite o mes do seu nascimento (MM): "))
+ano = int(input("Digite o ano do seu nascimento (AAAA): "))
+
+idade = 2026 - ano
+
+print(f'Sua data de nascimento é: {dia}/{mes}/{ano} e sua idade é: {idade}')
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+total = n1 + n2
+mult = n1 * n2
+div = n1 / n2
+sub = n1 - n2
+
+print(f'A soma desses números são: {total}')
+print(f'A multiplicação desses números são: {mult}')
+print(f'A divisão desses números são: {div}')
+print(f'A subtração desses números são: {sub}')
